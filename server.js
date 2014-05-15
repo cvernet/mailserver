47ac2b45b08f764b58e16ab9b44cc6b9c5936cd1
cvernet <cvernet@gmail.com>
2014-05-15 07:32:21 +0000

    nodemailer

--- server.js
+++ server.js
@@ -130,9 +130,9 @@ var SampleApp = function() {
         // send mail with defined transport object
         smtpTransport.sendMail(mailOptions, function(error, response){
             if(error){
-                console.log(error);
+                res.send(error);
             }else{
-                console.log("Message sent: " + response.message);
+                res.send("Message sent: " + response.message);
             }
 
             // if you don't want to use this transport object anymore, uncomment following line
