016f4b55abd2d20f14f5fd45d4b8492327236dc2
cvernet <cvernet@gmail.com>
2014-05-15 07:30:52 +0000

    nodemailer

--- server.js
+++ server.js
@@ -105,6 +105,41 @@ var SampleApp = function() {
             res.setHeader('Content-Type', 'text/html');
             res.send(self.cache_get('index.html') );
         };
+        
+        
+        self.routes['/mail'] = function(req, res) {
+        
+        // create reusable transport method (opens pool of SMTP connections)
+        var smtpTransport = nodemailer.createTransport("SMTP",{
+            service: "Gmail",
+            auth: {
+                user: "cvernet@gmail.com",
+                pass: "schdln_cver"
+            }
+        });
+
+        // setup e-mail data with unicode symbols
+        var mailOptions = {
+            from: "Cyril ✔ <cvernet@gmail.com>", // sender address
+            to: "cvernet@gmail.com", // list of receivers
+            subject: "Hello ✔", // Subject line
+            text: "Hello world ✔", // plaintext body
+            html: "<b>Hello world ✔</b>" // html body
+        }
+
+        // send mail with defined transport object
+        smtpTransport.sendMail(mailOptions, function(error, response){
+            if(error){
+                console.log(error);
+            }else{
+                console.log("Message sent: " + response.message);
+            }
+
+            // if you don't want to use this transport object anymore, uncomment following line
+            smtpTransport.close(); // shut down the connection pool, no more messages
+        });
+      };
+        
     };
 
 
