c5b213b727ac5435b3c4f424377f65bfa7d40804
cvernet <cvernet@gmail.com>
2014-05-24 22:12:10 +0200

    change sent access

--- server.js
+++ server.js
@@ -232,7 +232,8 @@ var SampleApp = function() {
       };
           
       self.routes['/imapsent'] = function(req, res) {
-          res.setHeader('Access-Control-Allow-Origin', 'http://cvernet.host-ed.me');
+          //res.setHeader('Access-Control-Allow-Origin', 'http://cvernet.host-ed.me');
+          res.setHeader('Access-Control-Allow-Origin', '*');
           res.setHeader('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
           if (req.method == 'OPTIONS') {res.send("200");}
               
